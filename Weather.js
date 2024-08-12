2d22ee7296d24a908d97e9ed254f3feb1ffd79a7
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-12 23:18:33 +0530

    add

--- Weather.js
+++ Weather.js
@@ -1,5 +1,5 @@
 const apiKey = "6e8b5c67c6e4c94da94d6715fb76f33c";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -16,8 +16,7 @@ async function checkWeather() {
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
