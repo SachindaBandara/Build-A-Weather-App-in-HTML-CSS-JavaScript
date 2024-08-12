a5332d2d871cc34348b303898796e42422dc8c20
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-12 22:28:19 +0530

    add

---
+++ Weather.js
@@ -0,0 +1,7 @@
+const apiKey = "6e8b5c67c6e4c94da94d6715fb76f33c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+
+async function checkWeather() {
+     
+}
